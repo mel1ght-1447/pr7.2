4e51e1bfbcada6e13c63071d599abd34402b1a63
mel1ght-1447 <maximbykov27@gmail.com>
2026-06-23 13:42:31 +0300

    Add files via upload

--- pr7.2/Calculation.xaml.cs
+++ pr7.2/Calculation.xaml.cs
@@ -32,13 +32,8 @@ namespace pr7._2
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            if (rBSquare.IsChecked == true)
-            {
-                CalcItem.IsEnabled = true;
-            }
-            else
-                CalcItem.IsEnabled = false;
             this.PreviewKeyDown += Preview;
+
         }
         private void Preview(object sender, KeyEventArgs e)
         {
@@ -57,7 +52,7 @@ namespace pr7._2
         }
         private void CalcItem_Click(object sender, RoutedEventArgs e)
         {
-            if (rBSquare.IsChecked == true)
+            if (Square.IsChecked == true)
             {
                 CalcItem.IsEnabled = true;
             }
@@ -67,7 +62,6 @@ namespace pr7._2
                 CalcItem.IsEnabled = false;
                 return;
             }
-
             double.TryParse(tBDiagonal1.Text, out double diagonal1);
             double.TryParse(tBDiagonal2.Text, out double diagonal2);
             double.TryParse(tBAngle.Text, out double angle);
@@ -87,22 +81,30 @@ namespace pr7._2
                 double rad = angle * System.Math.PI / 180;
                 square = side * side * System.Math.Sin(rad);
                 method = "using side and angle";
-                MessageBox.Show($"Squre: {System.Math.Round(square,2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"Squre: {System.Math.Round(square, 2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
                 ClearTextBox();
             }
             else if (side > 0 && radius > 0)
             {
-                square = 2 * side * radius;
-                method = "using side and radius";
-                MessageBox.Show($"Squre: {System.Math.Round(square,2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
-                ClearTextBox();
+                if (radius >= (side / 2))
+                {
+                    MessageBox.Show($"The radius should be less than half the length of the side.", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                else
+                {
+                    square = 2 * side * radius;
+                    method = "using side and radius";
+                    MessageBox.Show($"Squre: {System.Math.Round(square, 2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ClearTextBox();
+                }
 
             }
             else if (diagonal1 > 0 && diagonal2 > 0)
             {
                 square = diagonal1 * diagonal2 / 2;
                 method = "using diagonals";
-                MessageBox.Show($"Squre: {System.Math.Round(square,2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"Squre: {System.Math.Round(square, 2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
                 ClearTextBox();
             }
             else
@@ -114,6 +116,71 @@ namespace pr7._2
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            if (Square.IsChecked == true)
+            {
+                CalcItem.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show($"Turn on Square button!", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
+                CalcItem.IsEnabled = false;
+                return;
+            }
+            double.TryParse(tBDiagonal1.Text, out double diagonal1);
+            double.TryParse(tBDiagonal2.Text, out double diagonal2);
+            double.TryParse(tBAngle.Text, out double angle);
+            double.TryParse(tBSide.Text, out double side);
+            double.TryParse(tBRadius.Text, out double radius);
+
+
+            double square = 0;
+            string method = null;
+            if (diagonal1 > 0 && diagonal2 > 0 && side > 0 && radius > 0 && angle > 0)
+            {
+                MessageBox.Show($"All parameters are entered!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ClearTextBox();
+            }
+            else if (angle > 0 && angle < 180 && side > 0)
+            {
+                double rad = angle * System.Math.PI / 180;
+                square = side * side * System.Math.Sin(rad);
+                method = "using side and angle";
+                MessageBox.Show($"Squre: {System.Math.Round(square, 2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearTextBox();
+            }
+            else if (side > 0 && radius > 0)
+            {
+                if (radius >= (side / 2))
+                {
+                    MessageBox.Show($"The radius should be less than half the length of the side.", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                else
+                {
+                    square = 2 * side * radius;
+                    method = "using side and radius";
+                    MessageBox.Show($"Squre: {System.Math.Round(square, 2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ClearTextBox();
+                }
+
+            }
+            else if (diagonal1 > 0 && diagonal2 > 0)
+            {
+                square = diagonal1 * diagonal2 / 2;
+                method = "using diagonals";
+                MessageBox.Show($"Squre: {System.Math.Round(square, 2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearTextBox();
+            }
+            else
+            {
+                MessageBox.Show($"Check the entered parameters for calculating the Square!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ClearTextBox();
+            }
         }
     }
 }
