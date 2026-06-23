684bfbe2c26afe3d979b4d58a8875b64e3f50654
mel1ght-1447 <maximbykov27@gmail.com>
2026-06-23 11:38:20 +0300

    Add files via upload

--- pr7.2/Calculation.xaml.cs
+++ pr7.2/Calculation.xaml.cs
@@ -63,47 +63,47 @@ namespace pr7._2
             }
             else
             {
+                MessageBox.Show($"Turn on Square button!", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
                 CalcItem.IsEnabled = false;
                 return;
             }
 
-            if (!double.TryParse(tBDiagonal1.Text, out double diagonal1) )
-            {
-                diagonal1 = 0;
-            }
-            if (!double.TryParse(tBDiagonal2.Text, out double diagonal2))
-                diagonal2 = 0;
-            if (!double.TryParse(tBAngle.Text, out double angle))
-                angle = 0;
-            if (!double.TryParse(tBSide.Text, out double side))
-                side = 0;
-            if (!double.TryParse(tBRadius.Text, out double radius))
-                radius = 0;
+            double.TryParse(tBDiagonal1.Text, out double diagonal1);
+            double.TryParse(tBDiagonal2.Text, out double diagonal2);
+            double.TryParse(tBAngle.Text, out double angle);
+            double.TryParse(tBSide.Text, out double side);
+            double.TryParse(tBRadius.Text, out double radius);
+
 
             double square = 0;
             string method = null;
-            if (diagonal1 > 0 && diagonal2 > 0 && angle == 0 && side == 0 && radius == 0)
+            if (diagonal1 > 0 && diagonal2 > 0 && side > 0 && radius > 0 && angle > 0)
             {
-                square = diagonal1 * diagonal2 / 2;
-                method = "using diagonals";
-                MessageBox.Show($"Squre: {square}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"All parameters are entered!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 ClearTextBox();
             }
-            else if (angle > 0 && angle < 180 && side > 0 && diagonal1 == 0 && diagonal2 == 0 && radius == 0)
+            else if (angle > 0 && angle < 180 && side > 0)
             {
                 double rad = angle * System.Math.PI / 180;
                 square = side * side * System.Math.Sin(rad);
                 method = "using side and angle";
-                MessageBox.Show($"Squre: {square}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"Squre: {System.Math.Round(square,2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
                 ClearTextBox();
             }
-            else if (side > 0 && radius > 0 && diagonal1 == 0 && diagonal2 == 0 && angle == 0)
+            else if (side > 0 && radius > 0)
             {
                 square = 2 * side * radius;
                 method = "using side and radius";
-                MessageBox.Show($"Squre: {square}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"Squre: {System.Math.Round(square,2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
                 ClearTextBox();
 
+            }
+            else if (diagonal1 > 0 && diagonal2 > 0)
+            {
+                square = diagonal1 * diagonal2 / 2;
+                method = "using diagonals";
+                MessageBox.Show($"Squre: {System.Math.Round(square,2)}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearTextBox();
             }
             else
             {
