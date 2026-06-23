df6b6e85752a5f581a00b64ed18a24bd00ca4f1d
mel1ght-1447 <maximbykov27@gmail.com>
2026-06-23 11:16:36 +0300

    Add files via upload

--- pr7.2/Calculation.xaml.cs
+++ pr7.2/Calculation.xaml.cs
@@ -20,22 +20,53 @@ namespace pr7._2
     /// </summary>
     public partial class Calculation : Window
     {
-
+        private void ClearTextBox()
+        {
+            tBAngle.Clear();
+            tBDiagonal1.Clear();
+            tBDiagonal2.Clear();
+            tBRadius.Clear();
+            tBSide.Clear();
+        }
         public Calculation()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            //if (rBSquare.IsChecked == false)
-            //{
-            //    CalcItem.IsEnabled = false;
-            //}
-            //else
-            //    CalcItem.IsEnabled = true;
+            if (rBSquare.IsChecked == true)
+            {
+                CalcItem.IsEnabled = true;
+            }
+            else
+                CalcItem.IsEnabled = false;
+            this.PreviewKeyDown += Preview;
         }
-
+        private void Preview(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                switch (e.Key)
+                {
+                    case Key.C:
+                        CalcItem_Click(sender, e);
+                        break;
+                    case Key.B:
+                        Back_Click(sender, e);
+                        break;
+                }
+            }
+        }
         private void CalcItem_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (rBSquare.IsChecked == true)
+            {
+                CalcItem.IsEnabled = true;
+            }
+            else
+            {
+                CalcItem.IsEnabled = false;
+                return;
+            }
+
             if (!double.TryParse(tBDiagonal1.Text, out double diagonal1) )
             {
                 diagonal1 = 0;
@@ -56,6 +87,7 @@ namespace pr7._2
                 square = diagonal1 * diagonal2 / 2;
                 method = "using diagonals";
                 MessageBox.Show($"Squre: {square}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearTextBox();
             }
             else if (angle > 0 && angle < 180 && side > 0 && diagonal1 == 0 && diagonal2 == 0 && radius == 0)
             {
@@ -63,15 +95,25 @@ namespace pr7._2
                 square = side * side * System.Math.Sin(rad);
                 method = "using side and angle";
                 MessageBox.Show($"Squre: {square}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearTextBox();
             }
             else if (side > 0 && radius > 0 && diagonal1 == 0 && diagonal2 == 0 && angle == 0)
             {
                 square = 2 * side * radius;
                 method = "using side and radius";
                 MessageBox.Show($"Squre: {square}\nMethod: {method}", "rhomb", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearTextBox();
+
             }
             else
+            {
                 MessageBox.Show($"Check the entered parameters for calculating the Square!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ClearTextBox();
+            }
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
